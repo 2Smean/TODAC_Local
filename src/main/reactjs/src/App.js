8786b5a2b9275b9193aa75c5fd2eb108530f598d
이승민 <136636643+2Smean@users.noreply.github.com>
2024-01-26 16:13:14 +0900

    first commit

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import mainImg from './image/s1.png';
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className='App'>
+      <img alt='' src={mainImg} />
+      <h2><b>스프링부터 + 리액트 CI/CD 성공 기원 !!! <br />한번에 가보자 !</b></h2>
+    </div>
+  );
+}
+
+export default App;
